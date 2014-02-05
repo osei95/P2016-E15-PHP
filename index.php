a72744906e714ee3fc83ff7537ac853813ab8b24
Florian Pygmalion <florianpygmalion@air-de-florian.hetic.net>
2014-02-05 18:03:01 +0100

    test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $f3 = require('inc/functions.php');
 $f3 = require('lib/base.php');
 $f3->config('config/config.ini');
 $f3->config('config/routes.ini');
-$f3->config('config/database.ini');
+$f3->config('config/daabase.ini');
 $f3->config('config/apis.ini');
 
 $f3->run();
