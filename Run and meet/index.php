e2d378533e015efcc22dbb0879dd557fddb85034
Théo <ac-theo-dc@hotmail.fr>
2014-02-12 17:58:29 +0100

    Intégration page profil

---
+++ Run and meet/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="utf-8" />
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>Meet & run</title>
+	  <meta name="description" content="">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="css/reset.css">
+	  <link rel="stylesheet" href="font/stylesheet.css">
+	  <link rel="stylesheet" href="css/style.css">
+	  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	  <script type="text/javascript" src="js/script.js"></script>
+	</head>
+	<body id="landing">
+		<header class="active">
+			<div class="contain">
+				<div class="fleft">
+					
+				</div>
+				<form class="fright">
+					<input type="text" name="firstname" placeholder="Adresse e-mail">
+					<input type="text" name="lastname" placeholder="Mot de passe">
+					<input type="submit" value="Connexion">
+				</form>
+			</div>
+		</header>
+		<section class="">
+			<div class="contain">
+				<div class="fleft">
+					<h1>RUN & MEET</h1>
+					<p>Le site des rencontres pour sportifs</p>
+				</div>
+				<div class="fright">
+					<h2>Rejoignez-nous !</h2>
+					<form class="fright">
+						Pseudonyme<input type="text" name="pseudo"><br>
+						Mot de passe<input type="text" name="mdp"><br>
+						Bracelet/App
+						<select>
+						   <option>Aucun</option> 
+						   <optgroup label="Bracelet"> 
+						      <option>Nike Fuelband</option> 
+						      <option>Jawbone Up</option> 
+						   </optgroup> 
+						   <optgroup label="Application"> 
+						      <option>App Fuelband</option> 
+						      <option>App Jawbone Up</option> 
+						   </optgroup> 
+						</select><br>
+						<input type="submit" value="JE M'INSCRIS">
+					</form>
+					<p>En continuant, tu confirmes avoir lu et accepté nos <a href="">Termes et Conditions</a>, notre <a href="">Politique de protection des données</a> ainsi que notre <a href="">Politique en matière de Cookies</a></p>
+				</div>
+			</div>
+		</section>
+		<section class="">
+			<div class="contain">
+				<div class="fleft">
+					<img src="images/bracelet.png" alt="">
+				</div>
+				<div class="fright">
+					<h2>UTILISEZ VOTRE BRACELET...</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+			</div>
+		</section>
+		<section class="">
+		    <div class="contain">
+				<div class="fleft">
+					<h2>ET FAITES DES RENCONTRES !</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+				<div class="fright">
+					<img src="images/ipad.png" alt="">
+				</div>
+			</div>
+		</section>
+		<footer>
+			<div class="contain">
+				<div>
+					<div>
+						<p>© 2014 Run & Meet - Tous droits réservés</p>
+					</div>
+					<div>
+						<p class="footer-title">Nous suivre sur</p>
+						<div>
+							<a href=""></a>
+							<a href=""></a>
+							<a href=""></a>
+						</div>
+					</div>
+					<div>
+						<div>
+							<p class="footer-title">A propos de Run & Meet</p>
+							<ul>
+								<li><a href="">A propos</a></li>
+								<li><a href="">Conditions d'utilisation</a></li>
+								<li><a href="">Protection des données</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ Run and meet/profil.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="utf-8" />
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>Meet & run</title>
+	  <meta name="description" content="">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="css/reset.css">
+	  <link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+	  <link rel="stylesheet" href="font/stylesheet.css">
+	  <link rel="stylesheet" href="css/style.css">
+	  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	  <script type="text/javascript" src="fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+	  <script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+	  <script type="text/javascript" src="js/script.js"></script>
+
+	</head>
+	<body id="profil">
+		<header class="active">
+			<div class="contain">
+				<div>
+					<a href=""></a>
+					<a href=""></a>
+					<a href=""></a>
+					<a href=""></a>
+				</div>
+				<a href="#">Déconnexion</a>
+			</div>
+		</header>
+		<section id="informations">
+			<div class="contain clearfix">
+				<div class="fleft">
+					<h1 class="titleN2">Gros Pygmalion</h1>
+					<span class="actif"></span>
+					<div><img src="images/profil1.jpg" alt=""></div>
+					<a id="suivre" href="#">Suivre</a>
+					<a id="discuter" href="#">Discuter</a>
+				</div>
+				<div class="fright">
+					<h2 class="titleN2">A propos</h2>
+					<p>“ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ”</p>
+				</div>
+			</div>
+		</section>
+		<section id="resume">
+			<div class="contain">
+				<table>
+				   <tr>
+				       <td colspan="3"><a href="#" class="titleN2">Mes photos</a></td>
+				       <td colspan="3"><a href="#" class="titleN2">Mon profil</a></td>
+				       <td colspan="3"><a href="#" class="titleN2">Mes rencontres</a></td>
+				   </tr>
+				   <tr>
+				       <td colspan="3">
+							<table class="photos-list">
+								<tr>
+									<td><a class="fancybox" rel="group" href="images/profil1.jpg"><img src="images/profil1.jpg" alt=""><div class="wrapper"><span>Voir la photo</span></div></a></td>
+									<td><a class="fancybox" rel="group" href="images/profil2.jpg"><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Voir la photo</span></div></a></td>
+									<td><a class="fancybox" rel="group" href="images/profil2.jpg"><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Voir la photo</span></div></a></td>
+								</tr>
+								<tr>					
+									<td><a class="fancybox" rel="group" href="images/profil1.jpg"><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Voir la photo</span></div></a></td>
+									<td><a class="fancybox" rel="group" href="images/profil1.jpg"><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Voir la photo</span></div></a></td>
+									<td><a class="fancybox" rel="group" href="images/profil1.jpg"><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Voir la photo</span></div></a></td>
+								</tr>
+							</table>
+				       </td>
+				       <td colspan="3">
+				       		<table cellspacing="0" class="info-list">
+								<tr>
+									<td>
+										<div>
+											<h3>Taille</h3>
+											<p>1,84 m</p>
+										</div>
+									</td>
+									<td>
+										<div>
+											<h3>Poids</h3>
+											<p>83 kg</p>
+										</div>
+									</td>
+									<td>
+										<div>
+											<h3>Age</h3>
+											<p>23 ans</p>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div>
+											<h3>Ville</h3>
+											<p>Paris</p>
+										</div>
+									</td>
+									<td>
+										<div>
+											<h3>Caractère</h3>
+											<p>Jovial</p>
+										</div>
+									</td>
+									<td>
+										<div>
+											<h3>Apparence</h3>
+											<p>Gros sac</p>
+										</div>
+									</td>
+								</tr>
+							</table>
+				       </td>
+				       <td colspan="3">
+					       	<table class="photos-list">
+								<tr>
+									<td><a href=""><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Machin <br> trucbidule</span></div></a></td>
+									<td><a href=""><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Machin <br> trucbidule</span></div></a></td>
+									<td><a href=""><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Machin <br> trucbidule</span></div></a></td>
+								</tr>
+								<tr>					
+									<td><a href=""><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Machin <br> trucbidule</span></div></a></td>
+									<td><a href=""><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Machin <br> trucbidule</span></div></a></td>
+									<td><a href=""><img src="images/profil2.jpg" alt=""><div class="wrapper"><span>Machin <br> trucbidule</span></div></a></td>
+								</tr>
+							</table>
+				       </td>
+				   </tr>
+				</table>
+			</div>
+		</section>
+		<section id="activity">
+			<div class="contain">
+				<h2 class="titleN2">Activité sportive</h2>
+				<div class="graphic"></div>
+				<div>
+					<div>
+						<h3><span>45 676</span><br>Calories perdues</h3>
+					</div>
+					<div><h3><span>127,56</span><br>Minutes d'activité physique</h3></div>
+				</div>
+			</div>
+		</section>
+		<section id="actuality">
+			<div class="contain">
+				<h2 class="titleN2">Mon actualité</h2>
+				<ul class="col"> 											<!-- DISPLAY NONE -->
+					<li><a href="#">Toutes mes actualités</a></li>
+					<li><a href="#">Distance</a></li>
+					<li><a href="#">Calories</a></li>
+					<li><a href="#">Durée</a></li>
+				</ul>
+				<div class="separe"><span></span></div>
+				<ul>
+					<li class="mon-actu clearfix">
+						<div class="fleft">
+							<div><img src="images/profil2.jpg" alt=""></div>
+						</div>
+						<div class="fleft">
+							<p><strong>John Williams</strong> a parcouru 17 kms en 1 heure et 13 minutes </p>
+							<a href="#">J'encourage</a>
+						</div>
+						<div class="fright">
+							<p>20/01/2014</p>
+							<span>18</span>
+						</div>
+					</li>
+					<li class="mon-actu clearfix">
+						<div class="fleft">
+							<div><img src="images/profil2.jpg" alt=""></div>
+						</div>
+						<div class="fleft">
+							<p><strong>John Williams</strong> a parcouru 17 kms en 1 heure et 13 minutes </p>
+							<a href="#">J'encourage</a>
+						</div>
+						<div class="fright">
+							<p>20/01/2014</p>
+							<span>18</span>
+						</div>
+					</li>
+					<li class="mon-actu clearfix">
+						<div class="fleft">
+							<div><img src="images/profil2.jpg" alt=""></div>
+						</div>
+						<div class="fleft">
+							<p><strong>John Williams</strong> a parcouru 17 kms en 1 heure et 13 minutes </p>
+							<a href="#">J'encourage</a>
+						</div>
+						<div class="fright">
+							<p>20/01/2014</p>
+							<span>18</span>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</section>
+	</body>
+</html>
