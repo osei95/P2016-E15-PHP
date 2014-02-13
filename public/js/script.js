9cea75fdfbf212406bb3583f47a7d1c5591fe62a
Théo <ac-theo-dc@hotmail.fr>
2014-02-13 10:30:21 +0100

    Modif de la landing

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
+    resize();
+
+    window.onresize = function() {
+        resize();
+    };
     
+    function resize(){
+        if(window.innerHeight>670){
+            $("body>section:nth-child(2)").css('height',window.innerHeight-60+'px');
+        }
+    }
+    
+    $('a[href^="index.php#"]').click(function() {  
+      link = $(this).attr('href'); 
+      if($(link ).length>=1)
+        hauteur=$(link).offset().top;
+      else
+        hauteur = $("section#"+link.substr(10,link.length-1)).offset().top;
+        console.log(hauteur);
+      $('body').animate({scrollTop: hauteur}, 600);
+      return false;  
+    });
+
     $(document).scroll(function() {
         if($(document).scrollTop()+$(window).height()>(860+250)){
             $("#landing>section:nth-child(3)>div>div>img").animate({right:'0', opacity:1}, 1500);
@@ -8,6 +30,7 @@ $(document).ready(function() {
             $("#landing>section:nth-child(4)>div>div>img").animate({left:'0', opacity:1}, 1500);
         }
     });
+
     $(document).ready(function() {
 		$(".fancybox").fancybox();
 	});
