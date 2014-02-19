8109c2ae13463cf38e8714e6cab6ec8a563de388
Erwan Martin <contact@erwanmartin.fr>
2014-02-19 10:31:03 +0100

    update import activity (import several days activity at the same time)

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -14,6 +14,18 @@
 		function home($f3){
 			if($f3->exists('SESSION.user')){
 				$this->tpl=array('sync'=>'dashboard.html');
+
+				/* Récupération des informations de l'utilisateur */
+				$user_model = new User_model();
+				$user = $user_model->getUserById(array('id' => $f3->get('SESSION.user.user_id')));
+				if($user){
+
+					$user_infos = $user->cast();
+					$f3->set('user', $user_infos);
+					
+				}else{
+					$this->tpl=array('sync'=>'404.html');
+				}
 			}
 		}
 		
