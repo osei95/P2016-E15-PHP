7d6b52a7305271eb359304f0f3852325de133a30
Erwan Martin <contact@erwanmartin.fr>
2014-02-20 00:10:06 +0100

    update dashboard page (adding dynamic elements)

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -14,6 +14,25 @@
 		function home($f3){
 			if($f3->exists('SESSION.user')){
 				$this->tpl=array('sync'=>'dashboard.html');
+
+				/* Récupération des informations de l'utilisateur */
+				$user_model = new User_model();
+				$user = $user_model->getUserById(array('id' => $f3->get('SESSION.user.user_id')));
+				if($user){
+
+					$user_infos = $user->cast();
+					$f3->set('user', $user_infos);
+
+					$goals = $user_model->getAllGoalsToByUserId(array('id' => $f3->get('SESSION.user.user_id')));
+					$goals_infos = array();
+					foreach($goals as $key => $g){
+						$goals_infos[$key] = $g->cast();
+					}
+					$f3->set('goals', $goals_infos);
+					
+				}else{
+					$this->tpl=array('sync'=>'404.html');
+				}
 			}
 		}
 		
