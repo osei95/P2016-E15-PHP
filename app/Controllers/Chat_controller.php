99a864a63ccc34ee8e54166bcc4e8bf24499f9eb
Erwan Martin <contact@erwanmartin.fr>
2014-02-21 09:53:04 +0100

    add chat page integration

---
+++ app/Controllers/Chat_controller.php
@@ -0,0 +1,16 @@
+<?php
+
+	class Chat_controller extends Controller{
+
+		public function __construct(){
+		    parent::__construct();
+		    $this->tpl=array('sync'=>'chat.html');
+		}
+
+		function main($f3){
+
+		}
+
+	}
+
+?>
