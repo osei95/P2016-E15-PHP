1e8aeb398fbc322d4230b06d22c1622396a62915
Erwan Martin <contact@erwanmartin.fr>
2014-02-23 19:11:35 +0100

    update server.js

--- js/server.js
+++ js/server.js
@@ -24,40 +24,52 @@ io.sockets.on('connection',function(socket){
 	/* --------------------------------- */
 	socket.on('loadPage',function(params){
 
-		current = {
-			user : {
-				token : params.user.token,
-				id : null
-			}
-		};
+		if(typeof params.user!='undefined' && typeof params.user.token!='undefined' && typeof params.page!='undefined' && typeof params.page.name!='undefined'){
 
-		var connection = mysql.createConnection(DB_INFOS);
-		connection.connect();
-		// On récupère l'id de l'utilisateur avec son token
-		// Penser à la sécurité de la requête !
-		connection.query('SELECT user_id FROM user WHERE user_key="'+current.user.token+'"', function(err, rows, fields) {
+			current = {
+				user : {
+					token : params.user.token,
+					id : null
+				}
+			};
 
-			// On enregiste l'utilisateur
-			current.user.id = rows[0]['user_id'];
+			var connection = mysql.createConnection(DB_INFOS);
+			connection.connect();
+			// On récupère l'id de l'utilisateur avec son token
+			// Penser à la sécurité de la requête !
+			connection.query('SELECT user_id FROM user WHERE user_key="'+current.user.token+'"', function(err, rows, fields) {
+				if (err) throw err;
 
-			var status = '403';
+				// On enregiste l'utilisateur
+				current.user.id = rows[0]['user_id'];
 
-			if(current.user.id!=null){
-				users[current.user.id] = {
-					socket : socket,
-					page : {
-						name : params.page.name,
-						params : {}
-					}
-				};
-				status = '200';
-			}
+				var status = '403';
 
-			socket.emit('loadedPage', {
-				status : status
+				if(current.user.id!=null){
+					users[current.user.id] = {
+						socket : socket,
+						page : {
+							name : params.page.name,
+							params : {}
+						}
+					};
+					status = '200';
+				}
+
+				socket.emit('loadedPage', {
+					status : status
+				});
+
+				if(params.page.name=='chat'){
+					// On supprime les notifications de type message
+					connection.query('DELETE FROM notification WHERE user_id='+current.user.id+' AND notification_type="message" AND notification_seen=0', function(err, rows, fields) {
+						connection.end();
+					});
+				}
+
+
 			});
-
-		});
+		}
 	});
 
 	socket.on('disconnect', function(){
@@ -75,66 +87,83 @@ io.sockets.on('connection',function(socket){
 	/* Connexion au chat */
 	socket.on('connectIM',function(params){
 
-		var connection = mysql.createConnection(DB_INFOS);
+		if(typeof params.to!='undefined' && typeof params.to.id!='undefined'){
 
-		current.im = {
-			to : {
-				id : params.to.id
-			}
-		};
+			var connection = mysql.createConnection(DB_INFOS);
 
-		// On enregistre avec qui l'utilisateur chat
-		users[current.user.id].page.params.to = params.to.id;
+			current.im = {
+				to : {
+					id : params.to.id
+				}
+			};
 
-		// On récupère les anciens messages
-		connection.connect();
-		connection.query('SET lc_time_names = "fr_FR"', function() {
-			connection.query('SELECT CASE WHEN message.message_from='+current.user.id+' THEN "me" ELSE "friend" END message_from_class, message.message_from, message.message_content, DATE_FORMAT(message.message_time,"%Hh%i") message_time, message.message_time message_datetime, DATE_FORMAT(message.message_time,"%W %d %M %Y") message_date FROM message WHERE (message.message_from='+current.user.id+' AND message.message_to='+current.im.to.id+') OR (message.message_from='+current.im.to.id+' AND message.message_to='+current.user.id+') ORDER BY message_datetime DESC', function(err, rows, fields) {
-				if (err) throw err;
-				socket.emit('receiveMessageIM', {
-					messages : rows
+			// On enregistre avec qui l'utilisateur chat
+			users[current.user.id].page.params.to = params.to.id;
+
+			// On récupère les anciens messages
+			connection.connect();
+			connection.query('SET lc_time_names = "fr_FR"', function() {
+				connection.query('SELECT CASE WHEN message.message_from='+current.user.id+' THEN "me" ELSE "friend" END message_from_class, message.message_from, message.message_content, DATE_FORMAT(message.message_time,"%Hh%i") message_time, message.message_time message_datetime, DATE_FORMAT(message.message_time,"%W %d %M %Y") message_date FROM message WHERE (message.message_from='+current.user.id+' AND message.message_to='+current.im.to.id+') OR (message.message_from='+current.im.to.id+' AND message.message_to='+current.user.id+') ORDER BY message_datetime DESC', function(err, rows, fields) {
+					if (err) throw err;
+					socket.emit('receiveMessageIM', {
+						messages : rows
+					});
+					connection.end();
 				});
-				connection.end();
 			});
-		});
-
+		}
 	});
 
 	/* Envoi d'un message */
 	socket.on('sendMessageIM',function(params){
 
-		var message = {};
-	  	message['message_content'] = params.message;
-	  	message['message_from'] = current.user.id;
-	  	message['message_from_class'] = 'me';
+		if(typeof params.message!='undefined'){
 
-	  	var currentDate = new Date();
-	  	message['message_time'] = currentDate.getHours()+'h'+currentDate.getMinutes();
-	  	message['message_date'] = date_fr(currentDate);
+			var message = {};
+		  	message['message_content'] = params.message;
+		  	message['message_from'] = current.user.id;
+		  	message['message_from_class'] = 'me';
 
-		var connection = mysql.createConnection(DB_INFOS);
-		connection.connect();
-		connection.query('INSERT INTO message (message_from, message_to, message_content) VALUES ('+current.user.id+', '+current.im.to.id+', "'+params.message+'")', function(err, rows, fields) {
-			if (err) throw err;
-			  	socket.emit('receiveMessageIM', {
-					messages : [message]
-				});
-			if(typeof(users[current.im.to.id])!='undefined'){
-				if(users[current.im.to.id].page.name='chat'){
-					if(users[current.im.to.id].page.params.to==current.user.id){
-						message['message_from_class'] = 'friend';
-						users[current.im.to.id].socket.emit('receiveMessageIM', {
-							messages : [message]
-						});
+		  	var currentDate = new Date();
+		  	message['message_time'] = currentDate.getHours()+'h'+currentDate.getMinutes();
+		  	message['message_date'] = date_fr(currentDate);
+
+			var connection = mysql.createConnection(DB_INFOS);
+			connection.connect();
+			connection.query('INSERT INTO message (message_from, message_to, message_content) VALUES ('+current.user.id+', '+current.im.to.id+', "'+params.message+'")', function(err, rows, fields) {
+				if (err) throw err;
+				  	socket.emit('receiveMessageIM', {
+						messages : [message]
+					});
+				if(typeof(users[current.im.to.id])!='undefined'){
+					if(users[current.im.to.id].page.name=='chat'){
+						if(users[current.im.to.id].page.params.to==current.user.id){
+							message['message_from_class'] = 'friend';
+							users[current.im.to.id].socket.emit('receiveMessageIM', {
+								messages : [message]
+							});
+						}else{
+							users[current.im.to.id].socket.emit('notifyMessageIM', {
+								from : current.user.id
+							});
+						}
 					}else{
-						users[current.im.to.id].socket.emit('notifyMessageIM', {
-							from : current.user.id
+						connection.query('SELECT notification_id FROM notification WHERE user_id='+current.im.to.id+' AND notification_type="message" AND notification_from='+current.user.id+' AND notification_seen=0', function(err, rows, fields) {
+							if (err) throw err;
+							if(rows.length==0){
+								connection.query('INSERT INTO notification (user_id, notification_type, notification_content, notification_from) VALUES ('+current.im.to.id+', "message", "", '+current.user.id+')', function(err, rows, fields) {
+									if (err) throw err;
+									users[current.im.to.id].socket.emit('receiveNotification', {
+										type : 'message'
+									});
+									connection.end();
+								});
+							}
 						});
 					}
 				}
-			}
-		});
-		connection.end();
+			});
+		}
 	});
 
 });
