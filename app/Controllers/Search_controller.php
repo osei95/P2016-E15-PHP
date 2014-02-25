7d5cc36dd8c79ec6988520640af2f839d1af5d4b
Erwan Martin <contact@erwanmartin.fr>
2014-02-25 17:45:14 +0100

    add notifications to search page

--- app/Controllers/Search_controller.php
+++ app/Controllers/Search_controller.php
@@ -12,7 +12,10 @@
 		}
 
 		public function main($f3){
+			$user_model = new User_model();
 			
+			$notifications = $user_model->getAllNotificationsByUserId(array('id' => $f3->get('SESSION.user.user_id')));
+			$f3->set('notifications', $notifications);
 		}
 		
 	}
