83a02f9593fe74e9621b8710f7084904e12aa27c
Erwan Martin <contact@erwanmartin.fr>
2014-02-25 15:40:30 +0100

    add search page integration

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
     resize();
+    $(".torangekm").ionRangeSlider({type:"double", postfix: " km"});
+    $(".torangeage").ionRangeSlider({type:"double", postfix: " ans"});
+    $(".torangetaille").ionRangeSlider({type:"double", postfix: " cm"});
+    $(".torangepoids").ionRangeSlider({type:"double", postfix: " kg"});
+    $(".torangecal").ionRangeSlider({type:"double", postfix: " kcal"});
+
+    $(function() {
+        $( "#accordion" ).accordion({
+            "collapsible":true,
+            active: 2
+        });
+    });
 
     window.onresize = function() {
         resize();
     };
-
+    
     function resize(){
         if(window.innerHeight>670){
             $("#landing>section:nth-child(2)").css('height',window.innerHeight-60+'px');
@@ -21,7 +33,7 @@ $(document).ready(function() {
       $('body').animate({scrollTop: hauteur}, 600);
       return false;  
     });
-    
+
     $(document).scroll(function() {
         if($(document).scrollTop()+$(window).height()>(860+250)){
             $("#landing>section:nth-child(3)>div>div>img").animate({right:'0', opacity:1}, 1500);
@@ -30,8 +42,8 @@ $(document).ready(function() {
             $("#landing>section:nth-child(4)>div>div>img").animate({left:'0', opacity:1}, 1500);
         }
     });
+
     $(document).ready(function() {
 		$(".fancybox").fancybox();
 	});
-
 });
