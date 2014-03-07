14c3e8e73a06a4dfc9d4394bfc737623021b75bf
osei95 <franckosei233@gmail.com>
2014-03-07 04:19:08 +0100

    kit rencontre 2

--- app/Views/rencontres.php
+++ app/Views/rencontres.php
@@ -7,16 +7,12 @@
 	  <meta name="description" content="">
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <link rel="stylesheet" href="css/reset.css">
-	  <link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+	  <link rel="stylesheet" href="font/stylesheet.css">
+	  <link rel="stylesheet" href="css/style.css">
 	  <link rel="stylesheet" href="css/ion.rangeSlider.skinNice.css">
 	  <link rel="stylesheet" href="css/ionrangeSlider.css">
-	  <link rel="stylesheet" href="font/stylesheet.css">
-	  <link rel="stylesheet" href="css/style-dash.css">
-	  <link rel="stylesheet" href="css/style.css">
 	  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 	  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-	  <script type="text/javascript" src="fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
-	  <script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
 	  <script src="js/ion.rangeSlider.min.js"></script>
 	  <script type="text/javascript" src="js/script.js"></script>
 	</head>
@@ -32,18 +28,54 @@
 				<a href="#">Déconnexion</a>
 			</div>
 		</header>
+		
+
 		<section class="">
 				<h1 class="titleN2">Liste des rencontres</h1>
 				
 						<nav>
 							<div class="contain clearfix">
-								<ul id="middle-nav" class="bottom-border">
+								<ul id="meeting-nav" class="bottom-border">
 									<li><a href="#">Mes rencontres</a></li>
 									<li><a class="actif" href="#">Mes objectifs</a></li>
 									<li><a href="#">Mes invitations</a></li>
+									<li><a href="#">Mes followers</a></li>
 								</ul>
 							</div>
 						</nav>
+		</section>
+		
+		
+			<section class="contain" id="result">
+				<div class="list-result clearfix">
+					<div>
+						<img src="/medias/users/<?php echo($r['user_id']); ?>/profil.jpg" alt="">
+						<div>
+							<h2>Franck Osei</h2>
+							<p>Sarcelles</p>
+							<p>23 ans</p>
+							<a href="#">Voir le profil</a>
+						</div>
+					</div>
+			
+				</div>
+			</section>
+			
+						<section class="contain" id="result">
+				<div class="list-result-homme clearfix">
+					<div>
+						<img src="/medias/users/<?php echo($r['user_id']); ?>/profil.jpg" alt="">
+						<div>
+							<h2>Franck Osei</h2>
+							<p>Sarcelles</p>
+							<p>23 ans</p>
+							<a href="#">Voir le profil</a>
+						</div>
+					</div>
+			
+				</div>
+			</section>
+		
 						
 				
 				<section class="objectifs-H">
@@ -54,7 +86,7 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="fix fleft">
-									<p class=""><strong>Nicole Scherzinger</strong> 
+									<p class=""><strong><a href="#">Nicole Scherzinger</a></strong> 
 									vous a fixé un objectif de <strong>15 kms</strong> à parcourir en <strong>20 jours</strong></p>
 								</div>
 								<div class="fright">
@@ -68,10 +100,11 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="normal fleft">
-									<p>Vous avez accepté le défi de <strong>Laury Thilleman</strong></p>
+									<p>Vous avez accepté le défi de <strong><a href="#">
+									Laury Thilleman</a></strong></p>
 								</div>
 								<div class="fright">
-									<a class="fancybox button" href="#detail1">Voir les détails</a>
+									<a href="#" class="button">Voir les détails</a>
 									
 								</div>
 							</li>
@@ -82,7 +115,8 @@
 								</div>
 								<div class="done fleft">
 									<p class="">
-									Vous n’avez pas rempli l’objectif de <strong>Noémie Lenoir</strong></p>
+									Vous n’avez pas rempli l’objectif de <strong><a href="#">
+									Noémie Lenoir</a></strong></p>
 
 							</li>
 							
@@ -91,7 +125,8 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="finish fleft">
-									<p>Vous avez rempli l’objectif de <strong>Mila Kunis</strong></p>
+									<p>Vous avez rempli l’objectif de <strong><a href="#">
+									Mila Kunis</a></strong></p>
 								</div>
 								<div class="fright">
 									<a href="#" class="chat">Discuter</a>
@@ -104,7 +139,7 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="fix fleft">
-									<p class=""><strong>Emily Ratajkowski</strong>  a refusé votre invitation à discuter</p>
+									<p class=""><strong><a href="#">Emily Ratajkowski</a></strong>  a refusé votre invitation à discuter</p>
 								</div>
 							
 							</li>
@@ -113,8 +148,6 @@
 				</section>
 				
 		</section>
-		
-		
 		
 		
 		
@@ -141,7 +174,7 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="fix fleft">
-									<p class=""><strong>Nicole Scherzinger</strong> 
+									<p class=""><strong><a href="#">Nicole Scherzinger</a></strong> 
 									souhaite discuter avec vous </p>
 								</div>
 								<div class="fright">
@@ -156,10 +189,10 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="normal fleft">
-									<p>Vous avez accepté le défi de <strong>Laury Thilleman</strong></p>
+									<p>Vous avez accepté le défi de <strong><a href="#">Laury Thilleman</a></strong></p>
 								</div>
 								<div class="fright">
-									<a class="fancybox button" href="#detail1">Voir les détails</a>
+									<a href="#" class="button">Voir les détails</a>
 									
 								</div>
 							</li>
@@ -170,7 +203,7 @@
 								</div>
 								<div class="done fleft">
 									<p class="">
-									Vous n’avez pas rempli l’objectif de <strong>Noémie Lenoir</strong></p>
+									Vous n’avez pas rempli l’objectif de <strong><a href="#">Noémie Lenoir</a></strong></p>
 
 							</li>
 							
@@ -179,7 +212,7 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="finish fleft">
-									<p>Vous avez rempli l’objectif de <strong>Mila Kunis</strong></p>
+									<p>Vous avez rempli l’objectif de <strong><a href="#">Mila Kunis</a></strong></p>
 								</div>
 								<div class="fright">
 									<a href="#" class="chat">Discuter</a>
@@ -192,7 +225,7 @@
 									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
 								</div>
 								<div class="fix fleft">
-									<p class=""><strong>Emily Ratajkowski</strong>  a refusé votre invitation à discuter</p>
+									<p class=""><strong><a href="#">Emily Ratajkowski</a></strong>  a refusé votre invitation à discuter</p>
 								</div>
 							
 							</li>
@@ -201,43 +234,7 @@
 				</section>
 				
 		</section>
-		
-		<div style="display:none">
-			<div id="detail1" class="obj-box clearfix">
-				<h2>Objectif à atteindre</h2>
-				<h3 class="cible">Votre cible</h3>
-				<p class="name">Emily Ratiohsky</p>
-				<div class="photo"><img src="images/profil2.jpg" alt=""></div>
-				<div class="taille">
-					<h4>Taille</h4>
-					<p>1,84 m</p>
-				</div>
-				<div class="poids">
-					<h4>Poids</h4>
-					<p>84 kg</p>
-				</div>
-				<div class="age">
-					<h4>Age</h4>
-					<p>26 ans</p>
-				</div>
-				<h3 class="objectif">Votre objectif</h3>
-				<form action=""></form>
-					<div class="line clearfix">
-						<div class="left"><p>Kilomètres à parcourir :</p></div>
-						<div class="right">
-							<input type="text" class="torangekmbis" name="rayon" value="1;200">
-						</div>
-					</div>
-					<div class="line clearfix">
-						<div class="left"><p>Durée de l'objectif :</p></div>
-						<div class="right">
-							<input type="text" class="torangejours" name="rayon" value="1;20">
-						</div>
-					</div>
-					<input type="submit" value="ENVOYER">
-				</form>
-			</div>
-		</div>
+
 		
 		<footer>
 			<div class="contain">
