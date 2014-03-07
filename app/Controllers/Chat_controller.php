d6cf3d41c3133c59dca8530a038c861d490b476e
Erwan Martin <contact@erwanmartin.fr>
2014-03-07 03:58:54 +0100

    update chat controller

--- app/Controllers/Chat_controller.php
+++ app/Controllers/Chat_controller.php
@@ -18,30 +18,27 @@
 
 				/* Si on doit charger une conversation */
 				if($f3->exists('PARAMS.username')){
+
 					$user_to = $user_model->getUserByUsername(array('username' => $f3->get('PARAMS.username')));
-				}else{
-					$chat_model = new Chat_model();
-					$last_message = $chat_model->getLastMessage(array('user_id'=>$f3->get('SESSION.user.user_id')));
-					if(is_array($last_message) && count($last_message)>0){
-						$user_last_message = ($last_message[0]->message_to==$f3->get('SESSION.user.user_id')?$last_message[0]->message_from:$last_message[0]->message_to);
-						$user_to = $user_model->getUserById(array('id' => $user_last_message));
-					}
-				}
+					if($user_to){
 
-				if(isset($user_to) && $user_to){
+						/* On vérifie que les 2 personnes sont en relation */
+						$relation = $user_model->isRelation(array('from'=>$user->user_id, 'to'=>$user_to->user_id));
+						if($relation){
 
-					/* On vérifie que les 2 personnes sont en relation */
-					$relation = $user_model->isRelation(array('from'=>$user->user_id, 'to'=>$user_to->user_id));
-					if($relation){
+							$user_to_infos = $user_to->cast();
 
-						$user_to_infos = $user_to->cast();
+							$chat_model = new Chat_model();
+							$messages = $chat_model->getUserMessages(array('user_from'=>$user->user_id, 'user_to'=>$user_to->user_id));
 
-						$chat_model = new Chat_model();
-						$messages = $chat_model->getUserMessages(array('user_from'=>$user->user_id, 'user_to'=>$user_to->user_id));
+							$conversation_infos = array('messages'=>$messages, 'user_from'=>$user_infos, 'user_to'=>$user_to_infos);
 
-						$conversation_infos = array('messages'=>$messages, 'user_from'=>$user_infos, 'user_to'=>$user_to_infos);
-
-						$f3->set('conversation', $conversation_infos);
+							$f3->set('conversation', $conversation_infos);
+						}else{
+							$this->tpl=array('sync'=>'404.html');
+						}
+					}else{
+						$this->tpl=array('sync'=>'404.html');
 					}
 				}
 
