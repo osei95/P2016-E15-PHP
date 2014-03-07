ad48f9b0705411a8db493474362368b8b948e2e7
Théo <ac-theo-dc@hotmail.fr>
2014-03-07 05:24:09 +0100

    home

---
+++ app/Views/rencontres.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="utf-8" />
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>Meet & run</title>
+	  <meta name="description" content="">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="css/reset.css">
+	  <link rel="stylesheet" href="font/stylesheet.css">
+	  <link rel="stylesheet" href="css/style.css">
+	  <link rel="stylesheet" href="css/ion.rangeSlider.skinNice.css">
+	  <link rel="stylesheet" href="css/ionrangeSlider.css">
+	  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	  <script src="js/ion.rangeSlider.min.js"></script>
+	  <script type="text/javascript" src="js/script.js"></script>
+	</head>
+	<body id="recherche">
+		<header class="active header_connected " id="2">
+			<div class="contain">
+				<div>
+					<a href=""></a>
+					<a href=""></a>
+					<a href=""><span>33</span></a>
+					<a href="" class="active"></a>
+				</div>
+				<a href="#">Déconnexion</a>
+			</div>
+		</header>
+		
+
+		<section class="">
+				<h1 class="titleN2">Liste des rencontres</h1>
+				
+						<nav>
+							<div class="contain clearfix">
+								<ul id="meeting-nav" class="bottom-border">
+									<li><a href="#">Mes rencontres</a></li>
+									<li><a class="actif" href="#">Mes objectifs</a></li>
+									<li><a href="#">Mes invitations</a></li>
+									<li><a href="#">Mes followers</a></li>
+								</ul>
+							</div>
+						</nav>
+		</section>
+		
+		
+			<section class="contain" id="result">
+				<div class="list-result clearfix">
+					<div>
+						<img src="/medias/users/<?php echo($r['user_id']); ?>/profil.jpg" alt="">
+						<div>
+							<h2>Franck Osei</h2>
+							<p>Sarcelles</p>
+							<p>23 ans</p>
+							<a href="#">Voir le profil</a>
+						</div>
+					</div>
+			
+				</div>
+			</section>
+			
+						<section class="contain" id="result">
+				<div class="list-result-homme clearfix">
+					<div>
+						<img src="/medias/users/<?php echo($r['user_id']); ?>/profil.jpg" alt="">
+						<div>
+							<h2>Franck Osei</h2>
+							<p>Sarcelles</p>
+							<p>23 ans</p>
+							<a href="#">Voir le profil</a>
+						</div>
+					</div>
+			
+				</div>
+			</section>
+		
+						
+				
+				<section class="objectifs-H">
+					<div class="contain objectifs-feed clearfix">
+						<ul>
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="fix fleft">
+									<p class=""><strong><a href="#">Nicole Scherzinger</a></strong> 
+									vous a fixé un objectif de <strong>15 kms</strong> à parcourir en <strong>20 jours</strong></p>
+								</div>
+								<div class="fright">
+									<a href="#" class="accept">Accepter</a>
+									<a class="refus" href="#">Refuser</a>
+								</div>
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="normal fleft">
+									<p>Vous avez accepté le défi de <strong><a href="#">
+									Laury Thilleman</a></strong></p>
+								</div>
+								<div class="fright">
+									<a href="#" class="button">Voir les détails</a>
+									
+								</div>
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="done fleft">
+									<p class="">
+									Vous n’avez pas rempli l’objectif de <strong><a href="#">
+									Noémie Lenoir</a></strong></p>
+
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="finish fleft">
+									<p>Vous avez rempli l’objectif de <strong><a href="#">
+									Mila Kunis</a></strong></p>
+								</div>
+								<div class="fright">
+									<a href="#" class="chat">Discuter</a>
+									
+								</div>
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="fix fleft">
+									<p class=""><strong><a href="#">Emily Ratajkowski</a></strong>  a refusé votre invitation à discuter</p>
+								</div>
+							
+							</li>
+						</ul>
+					</div>
+				</section>
+				
+		</section>
+		
+		
+		
+		
+		<section id="objectifs-F">
+				<h1 class="titleN2">Liste des rencontres</h1>
+				
+						<nav>
+							<div class="contain clearfix">
+								<ul id="middle-nav" class="bottom-border">
+									<li><a href="#">Mes rencontres</a></li>
+									<li><a class="actif" href="#">Mes followers</a></li>
+									<li><a href="#">Mes invitations</a></li>
+								</ul>
+							</div>
+						</nav>
+						
+				
+				<section>
+					<div class="contain objectifs-feed clearfix">
+						<ul>
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="fix fleft">
+									<p class=""><strong><a href="#">Nicole Scherzinger</a></strong> 
+									souhaite discuter avec vous </p>
+								</div>
+								<div class="fright">
+									<a href="#" class="accept">Accepter</a>
+									<a class="refus" href="#">Refuser</a>
+									<a class="target" href="#">Fixer un objectif</a>
+								</div>
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="normal fleft">
+									<p>Vous avez accepté le défi de <strong><a href="#">Laury Thilleman</a></strong></p>
+								</div>
+								<div class="fright">
+									<a href="#" class="button">Voir les détails</a>
+									
+								</div>
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="done fleft">
+									<p class="">
+									Vous n’avez pas rempli l’objectif de <strong><a href="#">Noémie Lenoir</a></strong></p>
+
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="finish fleft">
+									<p>Vous avez rempli l’objectif de <strong><a href="#">Mila Kunis</a></strong></p>
+								</div>
+								<div class="fright">
+									<a href="#" class="chat">Discuter</a>
+									
+								</div>
+							</li>
+							
+							<li class="mes-objectifs clearfix">
+								<div class="fleft">
+									<div><img src="/medias/users/72/profil.jpg" alt=""></div>
+								</div>
+								<div class="fix fleft">
+									<p class=""><strong><a href="#">Emily Ratajkowski</a></strong>  a refusé votre invitation à discuter</p>
+								</div>
+							
+							</li>
+						</ul>
+					</div>
+				</section>
+				
+		</section>
+
+		
+		<footer>
+			<div class="contain">
+				<div>
+					<div>
+						<p>© 2014 Run & Meet - Tous droits réservés</p>
+					</div>
+					<div>
+						<p class="footer-title">Nous suivre sur</p>
+						<div>
+							<a href=""></a>
+							<a href=""></a>
+							<a href=""></a>
+						</div>
+					</div>
+					<div>
+						<div>
+							<p class="footer-title">A propos de Run & Meet</p>
+							<ul>
+								<li><a href="">A propos</a></li>
+								<li><a href="">Conditions d'utilisation</a></li>
+								<li><a href="">Protection des données</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
