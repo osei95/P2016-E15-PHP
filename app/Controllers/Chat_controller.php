19d1f26b273e4d2f6f187d03bd3b6e4b876043f4
Erwan Martin <contact@erwanmartin.fr>
2014-03-07 02:23:54 +0100

    update chat page loading last conversation

--- app/Controllers/Chat_controller.php
+++ app/Controllers/Chat_controller.php
@@ -18,28 +18,35 @@
 
 				/* Si on doit charger une conversation */
 				if($f3->exists('PARAMS.username')){
-
 					$user_to = $user_model->getUserByUsername(array('username' => $f3->get('PARAMS.username')));
-					if($user_to){
+				}else{
+					$chat_model = new Chat_model();
+					$last_message = $chat_model->getLastMessage(array('user_id'=>$f3->get('SESSION.user.user_id')));
+					if(is_array($last_message) && count($last_message)>0){
+						$user_last_message = ($last_message[0]->message_to==$f3->get('SESSION.user.user_id')?$last_message[0]->message_from:$last_message[0]->message_to);
+						$user_to = $user_model->getUserById(array('id' => $user_last_message));
+					}
+				}
 
-						/* On vérifie que les 2 personnes sont en relation */
-						$relation = $user_model->isRelation(array('from'=>$user->user_id, 'to'=>$user_to->user_id));
-						if($relation){
+				if($user_to){
 
-							$user_to_infos = $user_to->cast();
+					/* On vérifie que les 2 personnes sont en relation */
+					$relation = $user_model->isRelation(array('from'=>$user->user_id, 'to'=>$user_to->user_id));
+					if($relation){
 
-							$chat_model = new Chat_model();
-							$messages = $chat_model->getUserMessages(array('user_from'=>$user->user_id, 'user_to'=>$user_to->user_id));
+						$user_to_infos = $user_to->cast();
 
-							$conversation_infos = array('messages'=>$messages, 'user_from'=>$user_infos, 'user_to'=>$user_to_infos);
+						$chat_model = new Chat_model();
+						$messages = $chat_model->getUserMessages(array('user_from'=>$user->user_id, 'user_to'=>$user_to->user_id));
 
-							$f3->set('conversation', $conversation_infos);
-						}else{
-							$this->tpl=array('sync'=>'404.html');
-						}
+						$conversation_infos = array('messages'=>$messages, 'user_from'=>$user_infos, 'user_to'=>$user_to_infos);
+
+						$f3->set('conversation', $conversation_infos);
 					}else{
 						$this->tpl=array('sync'=>'404.html');
 					}
+				}else{
+					$this->tpl=array('sync'=>'404.html');
 				}
 
 				/* Récupération de toutes les relations */
--- app/Models/Chat_model.php
+++ app/Models/Chat_model.php
@@ -14,6 +14,10 @@
 			$this->dB->exec('SET lc_time_names = "fr_FR"');
 			return $this->dB->exec('SELECT CASE WHEN message.message_from=:user_from THEN "me" ELSE "friend" END message_from_class, message.message_from, message.message_content, DATE_FORMAT(message.message_time,"%Hh%i") message_time, message.message_time message_datetime, DATE_FORMAT(message.message_time,"%W %d %M %Y") message_date, message_from FROM message WHERE (message.message_from=:user_from AND message.message_to=:user_to) OR (message.message_from=:user_to AND message.message_to=:user_from) ORDER BY message_datetime', array(':user_from'=>$params['user_from'], ':user_to'=>$params['user_to']));
 		}
+
+		function getLastMessage($params){
+			return $this->mapper->find(array('message_to=? OR message_from=?', $params['user_id'], $params['user_id']), array('order'=>'message_time DESC', 'limit'=>1, 'offset' => 0));
+		}
 	}
 
 ?>
